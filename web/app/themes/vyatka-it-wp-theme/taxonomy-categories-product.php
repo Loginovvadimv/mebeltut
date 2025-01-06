0aac5c3cc87669bfcb72c11bb22b2e74e095a5d0
ParniskaVad <loginovvadimv@gmail.com>
2025-01-06 15:36:01 +0300

    home 0601

--- web/app/themes/vyatka-it-wp-theme/taxonomy-categories-product.php
+++ web/app/themes/vyatka-it-wp-theme/taxonomy-categories-product.php
@@ -3,7 +3,17 @@ use Timber\Helper;
 
 $context = \Timber\Timber::context();
 
+if (!isset($paged) || !$paged) {
+    $paged = 1;
+}
+
 $context['breadcrumbs'] = generate_crumbs();
 
+
+$context['posts'] = Timber::get_posts([
+    'post_type' => 'category.posts',
+    'posts_per_page' => '1',
+    'paged' => $paged
+]);
 
 Timber::render('page-allProducts.twig', $context);
